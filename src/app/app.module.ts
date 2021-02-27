2376adf8bdca72a6de47699247710ae3bec385b0
scorsaro <salvatore.corsaro@live.com>
2021-02-27 12:13:04 +0100

    added trainer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TrainerListComponent } from './components/trainer-list/trainer-list.component';
+import { TrainerDetailsComponent } from './components/trainer-details/trainer-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainerListComponent,
+    TrainerDetailsComponent
   ],
   imports: [
     BrowserModule
