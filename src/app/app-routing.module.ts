54970f74bd167e891c3e43cb3261f06b2d156d33
Celia <celiapdg@hotmail.com>
2021-03-02 19:42:06 +0100

    nothing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes } from '@angular/router';
 import {TrainerListComponent} from './components/trainer-list/trainer-list.component';
-import {TrainerDetailsComponent} from './components/trainer-details/trainer-details.component';
 
 const routes: Routes = [
   {
     path: 'trainer',
-    component: TrainerDetailsComponent
+    component: TrainerListComponent
   },
   {
     path: 'trainer/:trainerId',
-    component: TrainerDetailsComponent
+    component: TrainerListComponent
   }
 ];
 
