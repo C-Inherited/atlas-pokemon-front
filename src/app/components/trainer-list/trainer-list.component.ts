f089caa65434884fd67d88be8a15a2fa954f5e8b
Celia <celiapdg@hotmail.com>
2021-03-04 22:18:19 +0100

    carousel still not working

--- src/app/components/trainer-list/trainer-list.component.ts
+++ src/app/components/trainer-list/trainer-list.component.ts
@@ -12,6 +12,7 @@ export class TrainerListComponent implements OnInit {
   trainers: Trainer[] = [];
   numberOfColumns: number;
 
+  trainersCarousel: Trainer[][] = [];
   openForm = false;
 
   @Output() showTrainerDetailsEvent = new EventEmitter();
@@ -26,6 +27,25 @@ export class TrainerListComponent implements OnInit {
     this.trainers = [];
     await this.trainerService.getTrainers().then((trainerList) => {
         this.trainers = trainerList;
+        this.trainersCarousel = [];
+        let carouselPages: number = Math.floor(this.trainers.length/4)
+        let lastPageTrainers: number = this.trainers.length%4
+        for (let i=0; i<carouselPages; i++){
+          let trainersPage: Trainer[] = [];
+          for (let j=0; j<4; j++){
+            trainersPage.push(this.trainers[i*4+j])
+          }
+          this.trainersCarousel.push(trainersPage)
+        }
+        if (lastPageTrainers > 0){
+          let trainersPage: Trainer[] = [];
+        for (let i=(carouselPages)*4; i<(carouselPages)*4+lastPageTrainers; i++){
+          trainersPage.push(this.trainers[i])
+        }
+        console.log(trainersPage)
+        this.trainersCarousel.push(trainersPage)
+        }
+        
       });
     return this.trainers;
   }
