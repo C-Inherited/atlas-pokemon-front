97e0df70dfa50424e83ce285661d81b267f9ae8c
scorsaro <salvatore.corsaro@live.com>
2021-03-05 14:12:08 +0100

    deployed on heroku : youtube

--- src/app/components/home-component/home-component.component.ts
+++ src/app/components/home-component/home-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home-component',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponentComponent implements OnInit {
 
-  constructor() { }
+url: string = 'https://youtu.be/wvy-pO65GhQ?autoplay=1&mute=1&loop=1&controls=0';
+safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  constructor(private sanitizer: DomSanitizer) {
+
+  }
 
   ngOnInit(): void {
   }
